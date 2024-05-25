8914e465f7bffa3861d9dca577098f53f2f63d97
TebogoTete <tebogogp1991@gmail.com>
2024-05-25 14:40:04 +0200

    Added service section

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 
 import Header from './components/Header/Header';
 import Hero from './components/UI/Hero';
+import Services from './components/UI/Services';
 
 function App() {
   return (
     <>
     <Header />
-    <Hero/>
+    <Hero />
+    <Services/>
     </>
   );
 }
